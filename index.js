c6bf9c2caa90a2ad1add363a927defe5c9450ab7
mithun950 <mithunrony46@gmail.com>
2024-09-23 23:19:15 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getInputValueById(id) {
   const inputValue = parseFloat(document.getElementById(id).value);
   if (isNaN(inputValue) || inputValue === "" || inputValue <= 0) {
-    alert("Invalid Value! pleace Type valid number");
+    alert("Invalid Donation Amount");
   } else {
     alert("Congratulation You Donate For Nouakhali Flood");
   }
@@ -10,43 +10,84 @@ function getInputValueById(id) {
 }
 
 function getPreviousAmount(id) {
-    const yourAmount = parseFloat(document.getElementById(id).innerText);
-    return yourAmount;
+  return parseFloat(document.getElementById(id).innerText);
+    
 }
 
 //noakhali donate
     const noakhaliDonateButton = document.getElementById("noakhali-donate-button");
     noakhaliDonateButton.addEventListener("click", function () {
+      
+      const noakhaliInputValue = getInputValueById('nouakhali-input-value'); 
 
-    const noakhaliInputValue = getInputValueById("nouakhali-input-value");
-    const noakhaliDonateAmount = document.getElementById("noakhali-donate-amount");
-    noakhaliDonateAmount.innerText = noakhaliInputValue;
+      const noakhaliDonateAmountValue = document.getElementById('noakhali-donate-amount');
+      const noakhaliCurrentDonateAmount = parseFloat(noakhaliDonateAmountValue.innerText);
+          
+      const newAmountAddNoakhali = noakhaliCurrentDonateAmount + parseFloat(noakhaliInputValue);
+      noakhaliDonateAmountValue.innerText = newAmountAddNoakhali;
 
     const yourAmountValue = getPreviousAmount('your-amount')
-    const newTotalBalance = yourAmountValue - parseFloat(noakhaliDonateAmount);
+    const newTotalBalance = yourAmountValue - parseFloat(noakhaliInputValue);
     document.getElementById('your-amount').innerText = newTotalBalance;
 });
 
 // // feni donate
 
-    const feniDonateButton = document.getElementById("feni-donate-button");
-    feniDonateButton.addEventListener("click", function () {
-    const feniInputValue = getInputValueById("feni-input-value");
+const feniDonateButton = document.getElementById("feni-donate-button");
+feniDonateButton.addEventListener("click", function () {
+  
+  const feniInputValue = getInputValueById('feni-input-value'); 
 
-    const feniDonateAmount = document.getElementById("feni-donate-amount");
-    feniDonateAmount.innerText = feniInputValue;
+  const feniDonateAmountValue = document.getElementById('feni-donate-amount');
+  const feniCurrentDonateAmount = parseFloat(feniDonateAmountValue.innerText);
+      
+  const newAmountAddfeni = feniCurrentDonateAmount + parseFloat(feniInputValue);
+  feniDonateAmountValue.innerText = newAmountAddfeni;
+
+const yourAmountValue = getPreviousAmount('your-amount')
+const newTotalBalance = yourAmountValue - parseFloat(feniInputValue);
+document.getElementById('your-amount').innerText = newTotalBalance;
 });
+
+
 
 //  quota donate
-    const quotaDonateButton = document.getElementById("quota-donate-button");
-    quotaDonateButton.addEventListener("click", function () {
-    let quotaInputValue = getInputValueById("quota-input-value");
+const quotaDonateButton = document.getElementById("quota-donate-button");
+quotaDonateButton.addEventListener("click", function () {
+  
+  const quotaInputValue = getInputValueById('quota-input-value'); 
 
-    const quotaDonateAmount = document.getElementById("quota-donate-amount");
-    quotaDonateAmount.innerText = quotaInputValue;
+  const quotaDonateAmountValue = document.getElementById('quota-donate-amount');
+  const quotaCurrentDonateAmount = parseFloat(quotaDonateAmountValue.innerText);
+      
+  const newAmountAddquota = quotaCurrentDonateAmount + parseFloat(quotaInputValue);
+  quotaDonateAmountValue.innerText = newAmountAddquota;
+
+const yourAmountValue = getPreviousAmount('your-amount')
+const newTotalBalance = yourAmountValue - parseFloat(quotaInputValue);
+document.getElementById('your-amount').innerText = newTotalBalance;
 });
-        // history button and donate button toggle
-    const historyTab = document.getElementById("history-tab");
+// blog button 
+ 
+
+// home-btn
+const homeBtn = document.getElementById('home-btn');
+if(homeBtn){
+  homeBtn.addEventListener('click', function(){
+  window.location.href ='index.html';
+  });
+}
+// const blogBtn = document.getElementById('blog-btn')
+//  if(blogBtn){
+//   blogBtn.addEventListener('click', function(){
+//   window.location.href ='blog-btn.html';
+// });
+//  };
+
+
+  // history button and donate button toggle
+    
+  const historyTab = document.getElementById("history-tab");
     const donateTab = document.getElementById("donation-tab");
     historyTab.addEventListener("click", function () {
     historyTab.classList.add("bg-[#B4F461]", "font-bold");
